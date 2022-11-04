12aa96bae142aefa4964fa839b0c8f0522691469
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-11-04 15:46:18 +0700

    membuat array

--- Excercise2_Muhammad Fahrizal_126/Program.cs
+++ Excercise2_Muhammad Fahrizal_126/Program.cs
@@ -8,6 +8,9 @@ namespace Excercise2_Muhammad_Fahrizal_126
 {
     class Program
     {
+        // membuat array 
+        int[] fahrizal = new int[20];
+
         static void Main(string[] args)
         {
         }
